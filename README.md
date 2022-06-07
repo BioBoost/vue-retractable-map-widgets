70bae001a79bca89cb670e45bc146547ec587a87
Nico De Witte <nico.dewitte@outlook.com>
2022-06-07 21:48:57 +0200

    Using expansion-panel

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # vue-retractable-map-widgets
 
+Tried different approaches:
+
+* Map Dialog or overlay - looks messy + both need absolute heights / widths
+* Map between widgets / growing / shrinking - not very nice + complex
+* Best option seemed to be using a `v-expansion-panel`.
+
+Without map
+
+![](./img/map_hidden.png)
+
+With map
+
+![](./img/map_shown.png)
+
 ## Project setup
 ```
 yarn install
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,71 +1,57 @@
 <template>
-  <v-sheet>
-    <v-row dense>
 
-      <v-col cols="auto">
-        <v-card color="primary" class="d-flex justify-center align-center rounded-0" height="100%" hover @click="show_map=!show_map">
-          <v-icon class="mx-2" size="x-large">mdi-map-marker</v-icon>
+<v-container>  
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-card class="d-flex flex-column pa-3 bg-grey-lighten-4">
+          <v-card-title class="justify-center">
+            {{ widgets[0].value }}
+          </v-card-title>
         </v-card>
       </v-col>
 
-      <v-col cols="11">
-        <div>
-        <v-dialog
-          v-model="show_map"
-        >
-          <v-card>
-            <v-card-title>Sensor Map</v-card-title>
-              <v-img src="https://cdn.vuetifyjs.com/images/cards/forest-art.jpg" min-width="600"></v-img>
-            <v-card-text>
-              Please select a sensor from the map.
-            </v-card-text>
-          </v-card>
-        </v-dialog>
-        </div>
+      <v-col cols="12">
+        <v-expansion-panels>
+          <v-expansion-panel>
+            <v-expansion-panel-title class="d-flex justify-center align-center" hide-icons>
+              <v-icon color="error" icon="mdi-map-marker" class="mr-4"></v-icon> Click to toggle the map 
+            </v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <v-img src="https://media.wired.com/photos/59269cd37034dc5f91bec0f1/191:100/w_1280,c_limit/GoogleMapTA.jpg" />
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+    </v-row>
 
-        <v-container>  
+    <v-row class="my-10">
+      <v-col cols="12" class="text-center">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
+      </v-col>
+    </v-row>
 
-          <v-row>
-            <v-col cols="12">
-              <v-card class="d-flex flex-column pa-3 bg-grey-lighten-4">
-                <v-card-title class="justify-center">
-                  {{ widgets[0].value }}
-                </v-card-title>
-              </v-card>
-            </v-col>
-          </v-row>
-
-          <v-row class="my-10">
-            <v-col cols="12" class="text-center">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
-            </v-col>
-          </v-row>
-
-          <v-row align="center" justify="center">
-            <v-col cols="12" sm="6" md="4" v-for="widget in widgets" :key="widget.title">
-              <v-card color="grey-lighten-3 d-flex flex-column">
-                <v-card-title class="justify-center">{{ widget.title }}</v-card-title>
-                <v-card-text class="text-center"><v-icon size="x-large">{{ widget.icon }}</v-icon></v-card-text>
-                <v-card-text class="text-center">{{ widget.value }}</v-card-text>
-              </v-card>
-            </v-col>
-
-          </v-row>
-
-          <v-row class="my-10">
-            <v-col cols="12" class="text-center">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
-            </v-col>
-          </v-row>
-
-        </v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="6" md="4" v-for="widget in widgets" :key="widget.title">
+        <v-card color="grey-lighten-3 d-flex flex-column">
+          <v-card-title class="justify-center">{{ widget.title }}</v-card-title>
+          <v-card-text class="text-center"><v-icon size="x-large">{{ widget.icon }}</v-icon></v-card-text>
+          <v-card-text class="text-center">{{ widget.value }}</v-card-text>
+        </v-card>
       </v-col>
 
     </v-row>
-  </v-sheet>
+
+    <v-row class="my-10">
+      <v-col cols="12" class="text-center">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
+      </v-col>
+    </v-row>
+
+  </v-container>
+
 </template>
 
 <script>
@@ -116,7 +102,6 @@ export default {
         icon: 'mdi-signal-variant'
       },
     ],
-    show_map: false
   }),
 }
 </script>

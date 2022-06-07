8271515fc6101b8a8e991460d48d2c07bdf43af4
Nico De Witte <nico.dewitte@outlook.com>
2022-06-07 21:31:13 +0200

    Using a dialog

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,20 +1,34 @@
 <template>
   <v-sheet>
-    <v-row >
+    <v-row dense>
 
-      <!-- <v-col cols="auto">
+      <v-col cols="auto">
         <v-card color="primary" class="d-flex justify-center align-center rounded-0" height="100%" hover @click="show_map=!show_map">
           <v-icon class="mx-2" size="x-large">mdi-map-marker</v-icon>
         </v-card>
-      </v-col> -->
+      </v-col>
 
-      <v-col cols="12">
+      <v-col cols="11">
+        <div>
+        <v-dialog
+          v-model="show_map"
+        >
+          <v-card>
+            <v-card-title>Sensor Map</v-card-title>
+              <v-img src="https://cdn.vuetifyjs.com/images/cards/forest-art.jpg" min-width="600"></v-img>
+            <v-card-text>
+              Please select a sensor from the map.
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+        </div>
+
         <v-container>  
 
           <v-row>
             <v-col cols="12">
-              <v-card class="d-flex justify-center pa-3 bg-grey-lighten-4">
-                <v-card-title>
+              <v-card class="d-flex flex-column pa-3 bg-grey-lighten-4">
+                <v-card-title class="justify-center">
                   {{ widgets[0].value }}
                 </v-card-title>
               </v-card>
@@ -24,19 +38,18 @@
           <v-row class="my-10">
             <v-col cols="12" class="text-center">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure ducimus accusamus iste quasi, quo cumque placeat minus quaerat voluptatibus eaque quia veritatis soluta voluptatum dolor, non optio quibusdam quidem blanditiis?
             </v-col>
           </v-row>
 
           <v-row align="center" justify="center">
-            <v-col cols="12" md="3" offset-md="1" v-for="widget in widgets" :key="widget.title">
+            <v-col cols="12" sm="6" md="4" v-for="widget in widgets" :key="widget.title">
               <v-card color="grey-lighten-3 d-flex flex-column">
                 <v-card-title class="justify-center">{{ widget.title }}</v-card-title>
                 <v-card-text class="text-center"><v-icon size="x-large">{{ widget.icon }}</v-icon></v-card-text>
                 <v-card-text class="text-center">{{ widget.value }}</v-card-text>
               </v-card>
             </v-col>
+
           </v-row>
 
           <v-row class="my-10">
@@ -66,6 +79,11 @@ export default {
         title: "Name",
         value: "Soil Sensor Brugge",
         icon: 'mdi-ab-testing'
+      },
+      {
+        title: "Type",
+        value: "DUST-sensor",
+        icon: 'mdi-crosshairs-question'
       },
       {
         title: "Moisture @ -12cm",
@@ -98,7 +116,7 @@ export default {
         icon: 'mdi-signal-variant'
       },
     ],
-    show_map: true
+    show_map: false
   }),
 }
 </script>
